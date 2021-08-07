6252d5443d4641036fb2f8878b4be1d0eb3dca85
y-narikawa <hiyoko9513@gmail.com>
2021-08-07 21:30:40 +0900

    airの追加

--- hiyoko/main.go
+++ hiyoko/main.go
@@ -1,26 +1,54 @@
 package main
+
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm() 
-	fmt.Println("----Http.Request----")
-	fmt.Println("----line----")
-	fmt.Println(r.Method, r.RequestURI, r.Proto)
-	fmt.Println("----header----")
-	fmt.Println(r.Header)
-	fmt.Println("----body----")
-	fmt.Println(r.Body)
-	fmt.Println("----end----")
-	fmt.Fprintf(w, "hello go server!")
+
+type Task struct {
+	ID      int       `json:"id"`
+	Title   string    `json:"title"`
+	Content string    `json:"content"`
+	DueDate time.Time `json:"due_date"`
 }
+
+var tasks = []Task{{
+	ID:      1,
+	Title:   "A",
+	Content: "Aタスク",
+	DueDate: time.Now(),
+}, {
+	ID:      2,
+	Title:   "B",
+	Content: "Bタスク",
+	DueDate: time.Now(),
+}, {
+	ID:      3,
+	Title:   "C",
+	Content: "Cタスク",
+	DueDate: time.Now(),
+}}
+
 func main() {
-	http.HandleFunc("/", sayhelloName)
-	err := http.ListenAndServe(":8080", nil)
-	fmt.Println("Listen in 8080.")
-	if err != nil {
-		log.Fatal("Listen and server:", err)
+	handler1 := func(w http.ResponseWriter, r *http.Request) {
+		var buf bytes.Buffer
+		enc := json.NewEncoder(&buf)
+		if err := enc.Encode(&tasks); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(buf.String())
+
+		_, err := fmt.Fprint(w, buf.String())
+		if err != nil {
+			return
+		}
 	}
+
+	// GET /tasks
+	http.HandleFunc("/tasks", handler1)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
